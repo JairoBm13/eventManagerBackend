17ce47790398e1fb6b87be843c014e582c43cdb5
JairoBm13 <j.bautista.m13@outlook.com>
2019-08-16 01:49:08 -0500

    django done

--- project0/eventManager/views.py
+++ project0/eventManager/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 
-@require_http_methods()
+@require_http_methods(['GET'])
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 @require_http_methods(['POST'])
 def create_event(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls post.")
 
 @require_http_methods(['GET'])
 def event_details(request):
